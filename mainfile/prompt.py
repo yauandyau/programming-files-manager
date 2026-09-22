a4c477a15609223c7a06125afbbe0b425b830f50
yauandyau <158299480+yauandyau@users.noreply.github.com>
2026-09-22 12:48:59 +0800

    Create prompt.py

---
+++ mainfile/prompt.py
@@ -0,0 +1,35 @@
+DEFAULT_ROWS = {
+    'CSCI2040': {
+        'type': 'py',
+        'inpath': r'', 
+        'outpath': r'', 
+        'text': [
+            ['# Student Name :', '\n'], 
+            ['# Student ID   :', '\n'], 
+            ['# Class/Section:', 'CSCI2040\n'], 
+            ['# Date         :', '{date}\n']
+        ]
+    },
+    'ENGG1110': {
+        'type': 'c',
+        'inpath': r'', 
+        'outpath': r'', 
+        'text': [
+            [' * Student Name :', '\n'], 
+            [' * Student ID   :', '\n'], 
+            [' * Class/Section:', 'ENGG1110\n'], 
+            [' * Date         :', '{date}\n']
+        ]
+    },
+    'CSCI1130': {
+        'type': 'java',
+        'inpath': r'', 
+        'outpath': r'', 
+        'text': [
+            [' * Student Name :', '\n'], 
+            [' * Student ID   :', '\n'], 
+            [' * Class/Section:', 'CSCI1130\n'], 
+            [' * Date         :', '{date}\n']
+        ]
+    }
+}
